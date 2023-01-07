7debd134591ee8380c6b615f4c1adb1798a314ac
bpal13577 <bikash.pal@falconautoonline.com>
2023-01-07 13:08:58 +0530

    signup added and common css added

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+  
+  type:string="password";
+  isText:boolean=false;
+  eyeIcon:string="fa-solid fa-eye-slash";
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  hideShowPass(){
+    this.isText=!this.isText;
+    this.isText?this.type="text":this.type="password";
+    this.isText?this.eyeIcon="fa-solid fa-eye":this.eyeIcon="fa-solid fa-eye-slash";
   }
 
 }

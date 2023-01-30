849ac8ff87f42c7702d184e762b06da53618bb06
bikash-pal <bpal13577@gmail.com>
2023-01-31 02:17:54 +0530

    adding login signup and dashboard component

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import ValidateForm from 'src/app/helpers/validateform';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-signup',
@@ -8,19 +10,19 @@ import ValidateForm from 'src/app/helpers/validateform';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
-  
+
   type:string="password";
   isText:boolean=false;
   eyeIcon:string="fa-solid fa-eye-slash";
   signupForm!: FormGroup;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private auth: AuthService, private router:Router) { }
 
 
   ngOnInit(): void {
 
     this.signupForm=this.fb.group({
-      
+
       firstname:['',Validators.required],
       lastname:['',Validators.required],
       email:['',Validators.required],
@@ -38,6 +40,16 @@ export class SignupComponent implements OnInit {
   onSubmit(){
     if(this.signupForm.valid){
       console.log(this.signupForm.value);
+      this.auth.signUp(this.signupForm.value).subscribe({
+        next:(res)=>{
+          alert(res.message)
+          this.signupForm.reset();
+          this.router.navigate(['login']);
+        },
+        error:(err)=>{
+          alert(err.error.message)
+        }
+      });
     }
     else{
       console.log('this is not valid');
